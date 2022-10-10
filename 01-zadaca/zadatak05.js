a89e2d5cec4708b3a6de00afb2aa3b1d01bb5a38
gknapic <knapicgabrijela@gmail.com>
2022-10-10 22:10:45 +0200

    Rjesen Zadatak 5

---
+++ 01-zadaca/zadatak05.js
@@ -0,0 +1,13 @@
+//  Napiši funkciju kojoj se predaje polje brojeva, te ona ispisuje brojeve djeljive s 3
+
+var niz = [1,2,3,4,5,6,7,8,9,10];
+
+function djeljenje(){
+  for (let i of niz) {
+    if (i % 3 == 0) {
+      console.log("Broj djeljiv s 3 je: " + i);
+    }
+  }
+}
+
+djeljenje();
